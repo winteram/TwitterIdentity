c540633112d405a495e034877cf8977c0f4aa226
Winter Mason <winteram@gmail.com>
2012-06-22 17:39:10 -0400

    rearranged directory

---
+++ src/py/AuthAT.py
@@ -0,0 +1,4 @@
+oauth_token = "563118238-aVS68vGHeiWuoLCHIOudAPa6hmhnwIBsSkUfeBXt"
+oauth_secret = "M6h51pETL8CWkowEeyh6cb7gNpNTyBpl7fLJk45J4Y"
+CONSUMER_KEY = "PCMmY6ERIWJM9tgjIiQRwA"
+CONSUMER_SECRET = "YWeRQPivyjc9ZUSLQbaFj8enJviPZ8cw55mu3qSuJdk"
