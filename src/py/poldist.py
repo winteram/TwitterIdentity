fbe98477b1b01a7a2eb673245f495c900e988b7c
Winter Mason <winteram@gmail.com>
2012-08-03 13:42:45 -0400

    Further improved poldist

--- src/py/poldist.py
+++ src/py/poldist.py
@@ -6,6 +6,9 @@ from nltk import *
 from nltk.corpus import PlaintextCorpusReader
 from nltk.collocations import *
 
+bigram_measures = BigramAssocMeasures()
+trigram_measures = TrigramAssocMeasures()
+
 # parse corpus
 def parseCorpus(corpusdir):
     # Load the corpus into nltk and store files as separate file ID, which can be accessed by name.
@@ -15,6 +18,12 @@ def parseCorpus(corpusdir):
     corpusText = re.sub('[^a-zA-Z ]', '', corpusText)
     corpusText = re.sub('(?P<endword>[a-z]+)(?P<begword>[A-Z])', '\g<endword> \g<begword>', corpusText)
     corpusText = corpusText.lower()
+    # Create Frequency Distributions for bigrams
+    # Dem_bigrams = BigramCollocationFinder._ngram_freqdist(Dem_wordlist, 2)
+
+    # Create Frequency Distributions for trigrams
+    # Dem_trigrams = BigramCollocationFinder._ngram_freqdist(Dem_wordlist, 3)
+
     return word_tokenize(corpusText)
 
 # parse list of URLs
@@ -31,10 +40,14 @@ def parseURLs(urllist):
     urlText = re.sub('[^a-zA-Z ]', '', urlText)
     urlText = re.sub('(?P<endword>[a-z]+)(?P<begword>[A-Z])', '\g<endword> \g<begword>', urlText)
     urlText = urlText.lower()
+    # Create Frequency Distributions for bigrams
+    # Dem_bigrams = BigramCollocationFinder._ngram_freqdist(Dem_wordlist, 2)
+
+    # Create Frequency Distributions for trigrams
+    # Dem_trigrams = BigramCollocationFinder._ngram_freqdist(Dem_wordlist, 3)
+
     return word_tokenize(urlText)
 
-bigram_measures = BigramAssocMeasures()
-trigram_measures = TrigramAssocMeasures()
 
 # Get text for political party dictionaries (from corp and/or URLs)
 
@@ -54,50 +67,28 @@ listnames.append("Rep")
 wordlists.append(parseURLs(lpURLs))
 listnames.append("LP")
 
-#Create Frequency Distributions for each of these
-DemDist = FreqDist(Dem_wordlist)
-RepDist = FreqDist(Rep_wordlist)
-LPDist = FreqDist(lp_wordlist)
 
-# Create Frequency Distributions for bigrams
-#Dem_bigrams = BigramCollocationFinder._ngram_freqdist(Dem_wordlist, 2)
-
-# Create Frequency Distributions for trigrams
-#Dem_trigrams = BigramCollocationFinder._ngram_freqdist(Dem_wordlist, 3)
-
-# get proportions for each term
+# Input list of wordlists, return list of "score" dictionaries
 TotalList = {}
+wordProps = []
+for wordlist in wordlists:
+    wordDist = FreqDist(wordlist)
+    wordList = [(word,wordDist.freq(word)) for word in wordDist.keys()]
+    wordProp = {}
+    for word, prop in wordList:
+        wordProp.setdefault(word, []).append(prop)
+        if word in TotalList:
+            TotalList[word] += prop
+        else:
+            TotalList[word] = prop
+    wordProps.append(wordProp)
 
-DemList = [(word,DemDist.freq(word)) for word in DemDist.keys()]
-DemProp = {}
-for word, prop in DemList:
-    DemProp.setdefault(word, []).append(prop)
-    if word in TotalList:
-        TotalList[word] += prop
-    else:
-        TotalList[word] = prop
-
-RepList = [(word,RepDist.freq(word)) for word in RepDist.keys()]
-RepProp = {}
-for word, prop in RepList:
-    RepProp.setdefault(word, []).append(prop)
-    if word in TotalList:
-        TotalList[word] += prop
-    else:
-        TotalList[word] = prop
-
-LPList = [(word,LPDist.freq(word)) for word in LPDist.keys()]
-LPProp = {}
-for word, prop in LPList:
-    LPProp.setdefault(word, []).append(prop)
-    if word in TotalList:
-        TotalList[word] += prop
-    else:
-        TotalList[word] = prop
-
-DemRat = {}
-for word, prop in DemList:
-    DemRat[word] = prop / TotalList[word]
+# Normalize wordlists by frequencies across corpuses
+wordRats = []
+for wordProp in wordProps:
+    wordRat = {}
+    for word, prop in DemList:
+        DemRat[word] = prop / TotalList[word]
 
 RepRat = {}
 for word, prop in RepList:
