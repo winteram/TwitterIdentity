96a7fb8b10e987a67e44c0b9256af3edf081e5fc
Winter Mason <winteram@gmail.com>
2013-02-07 15:03:51 -0500

    Got group frequencies for followers

--- src/py/FollowerGroups.py
+++ src/py/FollowerGroups.py
@@ -2,6 +2,7 @@
 # 
 import pymysql
 import twitter
+import operator
 from encrypt import *
 
 tokens = get_tokens('GroupID_Project')
@@ -22,15 +23,22 @@ for follower in followers:
 conn = pymysql.connect(host='smallsocialsystems.com', port=3306, user='smalls7_groupid', passwd='LetsPublish!', db='smalls7_identity')
 cur = conn.cursor()
 
-flist = '"' + '","'.join(followers_encoded[0:5]) + '"'
+flist = '"' + '","'.join(followers_encoded) + '"'
 cur.execute('SELECT Id, username, own_form1, own_form3, own_form5 FROM survey WHERE username IN ('+flist+')')
 survey_followers = cur.fetchall()
 
 own_groups = {}
 for surveyer in survey_followers:
 	for sidx in range(2,5):
-		if surveyer[sidx].lower() in own_groups:
-			own_groups[surveyer[sidx].lower()] += 1
-		else:	
-			own_groups[surveyer[sidx].lower()] = 1
+		if surveyer[sidx] != None and surveyer[sidx] != '':
+			if surveyer[sidx].lower() in own_groups:
+				own_groups[surveyer[sidx].lower()] += 1
+			else:	
+				own_groups[surveyer[sidx].lower()] = 1
 
+# write output, for safe keeping
+fh = open('followgroups.csv','w')
+for k, v in own_groups.iteritems():
+	if k != '':
+		fh.write('%s,%d\n' % (k,v))
+fh.close()
