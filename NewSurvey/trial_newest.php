3164d8be3c1a96f34c42846d0f81b614d5ed1f5f
Winter Mason <winteram@gmail.com>
2020-05-20 13:53:48 -0400

    Updating jquery reference

--- NewSurvey/trial_newest.php
+++ NewSurvey/trial_newest.php
@@ -5,8 +5,8 @@
 	<link rel="shortcut icon" type="image/x-icon" href="core/images/idproj.ico" />
 	<title>Aspects of the Self</title>
 
-	<script src="core/jquery-ui-1.8.21.custom/js/jquery-1.7.2.min.js" type="text/javascript"></script>
-	<script src="core/jquery-ui-1.8.21.custom/js/jquery-ui-1.8.21.custom.min.js" type="text/javascript"></script>
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 	<script src="trial_newest.js" type="text/javascript"></script>
 	<script src="core/shuffle.js" type="text/javascript"></script>
 
